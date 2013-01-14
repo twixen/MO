c33fd9c8cad083736701382610480f17935558fc
Damian Soliński <twix88@gmail.com>
2013-01-14 12:37:43 +0100

    update

--- zad3/zad3.c
+++ zad3/zad3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-double fdifferential(double x, double y);
+double fdiff(double x, double y);
 double f(double x);
 
 int main() {
@@ -8,18 +8,21 @@ int main() {
     double euler = 0;
     double heun = 0;
     double x = 1;
-    scanf("%lf", &k);
-    printf("      x        f(x)      euler       blad       heun       blad  \n");
+    do {
+        printf("Podaj krok całkowania z przedziału (0,1): \n");
+        scanf("%lf", &k);
+    } while (k <= 0 || k >= 1);
+    printf("      x        f(x)      euler       błąd       heun       błąd  \n");
     while (x + k <= 3) {
-        euler += fdifferential(x, euler) * k;
-        heun += (fdifferential(x, heun) + fdifferential(x + k, heun + fdifferential(x, heun) * k)) * k / 2;
+        euler += fdiff(x, euler) * k;
+        heun += (fdiff(x, heun) + fdiff(x + k, heun + fdiff(x, heun) * k)) * k / 2;
         x += k;
         printf("%10.6lf %10.6lf %10.6lf %10.6lf %10.6lf %10.6lf\n", x, f(x), euler, fabs(euler - f(x)), heun, fabs(heun - f(x)));
     }
     return 0;
 }
 
-double fdifferential(double x, double y) {
+double fdiff(double x, double y) {
     return sqrt(y + x * x) + x * (4 * x * x - x - 2);
 }
 
